91876260f227d4827be6a1bb351aeb284b8118cb
Thomas Gusewelle <tgusewelle@gkwmedia.com>
2023-06-09 20:35:12 -0500

    expanded keywords

--- src/lexer/lexer.rs
+++ src/lexer/lexer.rs
@@ -11,6 +11,12 @@ pub enum Token {
 
     Assign,
     Plus,
+    Minus,
+    Bang,
+    Asterisk,
+    Slash,
+    GreaterThan,
+    LessThan,
 
     Comma,
     Semicolon,
@@ -22,6 +28,10 @@ pub enum Token {
 
     Function,
     Let,
+    If,
+    Return,
+    True,
+    False,
 }
 
 impl Display for Token {
@@ -35,6 +45,12 @@ impl Display for Token {
 
             Token::Assign => write!(f, "Assign"),
             Token::Plus => write!(f, "Plus"),
+            Token::Minus => write!(f, "Minus"),
+            Token::Bang => write!(f, "Bang"),
+            Token::Asterisk => write!(f, "Asterisk"),
+            Token::Slash => write!(f, "Slash"),
+            Token::GreaterThan => write!(f, "GreaterThan"),
+            Token::LessThan => write!(f, "LessThan"),
 
             Token::Comma => write!(f, "Commma"),
             Token::Semicolon => write!(f, "Semicolin"),
@@ -46,6 +62,10 @@ impl Display for Token {
 
             Token::Function => write!(f, "Function"),
             Token::Let => write!(f, "Let"),
+            Token::If => write!(f, "If"),
+            Token::Return => write!(f, "Return"),
+            Token::True => write!(f, "True"),
+            Token::False => write!(f, "False"),
         };
     }
 }
@@ -83,6 +103,10 @@ impl Lexer {
         return match ident.as_str() {
             "fn" => Token::Function,
             "let" => Token::Let,
+            "return" => Token::Return,
+            "if" => Token::If,
+            "true" => Token::True,
+            "false" => Token::False,
             _ => Token::Ident(ident),
         };
     }
@@ -123,6 +147,12 @@ impl Lexer {
             b'+' => Token::Plus,
             b'{' => Token::LBrace,
             b'}' => Token::RBrace,
+            b'!' => Token::Bang,
+            b'-' => Token::Minus,
+            b'*' => Token::Asterisk,
+            b'/' => Token::Slash,
+            b'<' => Token::LessThan,
+            b'>' => Token::GreaterThan,
             0 => Token::EOF,
             b'a'..=b'z' | b'A'..=b'Z' | b'_' => {
                 let ident: String = self.read_indetifier();
@@ -158,6 +188,14 @@ mod tests {
                 x + y;
             };
             let result = add(five, ten);
+            !-/*5;
+            5 < 10 > 5;
+            
+            if (5 < 10) {
+            return true;
+            } else {
+            return false;
+}
             "#;
         let mut lexer = Lexer::new(input.into());
 
@@ -197,6 +235,18 @@ mod tests {
             Token::Comma,
             Token::Ident(String::from("ten")),
             Token::RParen,
+            Token::Semicolon,
+            Token::Bang,
+            Token::Minus,
+            Token::Slash,
+            Token::Asterisk,
+            Token::Int(String::from("5")),
+            Token::Semicolon,
+            Token::Int(String::from("5")),
+            Token::LessThan,
+            Token::Int(String::from("10")),
+            Token::GreaterThan,
+            Token::Int(String::from("5")),
             Token::Semicolon,
         ];
 
